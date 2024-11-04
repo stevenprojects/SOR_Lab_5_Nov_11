ea08b99daebcb1e958ef35062eb5a1c344b01bb6
Steven O'Reilly <steven.oreilly2@mycit.ie>
2024-11-04 11:49:24 +0000

    First commit

---
+++ agile_processes_python/src/app.py
@@ -0,0 +1,12 @@
+def squares(x):
+    # Perform calculus
+    y = x ** 2
+    return y
+
+def cube(x):
+    # Perform calculus
+    y = x ** 3
+    return y
+
+if __name__ == "__main__":
+    print(f"The square of 20 is: {squares(20)}")
